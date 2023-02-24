d883f4d2feb3fe30eb749027f592e24055ae3e26
Moisés <guile@studiobrasuka.com.br>
2023-02-23 22:01:26 -0300

    ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,31 @@ function menu() {
   });
 }
 menu();
+
+function idiomas() {
+  const area = document.querySelector(".flags");
+  const menu = document.querySelector(".area__imgs");
+
+  area.addEventListener("click", () => {
+    area.classList.toggle("ativo");
+    menu.classList.toggle("ativo");
+  });
+}
+idiomas();
+
+function lazyLoad() {
+  const lazyImages = document.querySelectorAll("img.lazyload");
+  const windowHeight = window.innerHeight;
+
+  lazyImages.forEach((image) => {
+    const imageTop = image.getBoundingClientRect().top;
+    if (imageTop < windowHeight) {
+      image.src = image.dataset.src;
+      image.classList.remove("lazyload");
+    }
+  });
+}
+
+lazyLoad();
+
+window.addEventListener("scroll", lazyLoad);
